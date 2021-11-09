a1181231aeae3fd2fa5b59de95c5750ebd96a7b5
Paul-Bel <pn.beloborodov.msk@gmail.com>
2021-11-10 00:29:39 +0300

    Initialize project using Create React App

--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import store from "./Redux/Rudux_Store";
-import {Provider} from "react-redux";
+import reportWebVitals from './reportWebVitals';
 
-export const renderEnteerTree = () => {
-    ReactDOM.render(
-        <Provider store={store}>
-        <App
-            state={store.getState()}
-            dispatch={store.dispatch.bind(store)}
-        />,
-        </Provider>,
-        document.getElementById('root')
-    );
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-renderEnteerTree()
-store.subscribe(renderEnteerTree)
-
-// store.subscribe(renderEnteerTree)
-
-
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
