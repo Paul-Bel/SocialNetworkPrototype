b7385f26cc4913363650394206d36a3d970a5fb6
Paul-Bel <pn.beloborodov.msk@gmail.com>
2021-12-12 23:21:32 +0300

    add observer

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import {renderEnteerTree} from "./render";
-import {data} from "./components/Redax/State";
+import App from './App';
+import {addMyPost, changePost, data, subscribe} from "./components/Redax/State";
+
+type PostType = {
+    id: number,
+    message: string,
+    likescounte: number,
+}
+type DialogNickType = { id: number, name: string }
+type MessageType = { id: number, message: string }
+type DataType = {
+    profilePage: { post: PostType[], newPostText: string },
+    dialogNick: DialogNickType[],
+    messages: MessageType[],
+}
 
 
-renderEnteerTree(data);
+export const renderEnteerTree = (data: DataType) => {
+    ReactDOM.render(
+        <App
+            data={data}
+            addMyPost={addMyPost}
+            changePost={changePost}
+        />,
+
+        document.getElementById('root')
+    );
+}
+renderEnteerTree(data)
+subscribe(renderEnteerTree)
+
+
+
