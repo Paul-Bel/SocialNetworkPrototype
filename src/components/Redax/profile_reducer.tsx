15c5508bd1b78020dae5492d28868ad77f2b8e7b
Paul-Bel <pn.beloborodov.msk@gmail.com>
2021-12-23 02:37:22 +0300

    added reducer default

--- src/components/Redax/profile_reducer.tsx
+++ src/components/Redax/profile_reducer.tsx
@@ -20,18 +20,22 @@ const addPost = 'addPost'
 const changePost = 'changePost'
 
 const profileReducer = (action: ActionPropsType, state: DataType) => {
-        switch (action.type) {
-            case changePost:
-                if (action.value)
+    switch (action.type) {
+        case changePost:
+            if (action.value)
                 state.profilePage.newPostText = action.value;
-                return state;
-            case addPost:
-                if (state.profilePage.newPostText.trim()){
+            return state;
+        case addPost:
+            if (state.profilePage.newPostText.trim()) {
                 let addNewPost = {id: 6, message: state.profilePage.newPostText.trim(), likescounte: 0}
                 state.profilePage.post.push(addNewPost)
-                state.profilePage.newPostText = ''};
-                return state
-        }
+                state.profilePage.newPostText = ''
+            }
+            ;
+            return state;
+        default:
+            return state
+    }
 }
 
 export default profileReducer
