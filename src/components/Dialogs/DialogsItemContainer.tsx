ed0cd5b4d757878d6c2a3881c7b73142c4b508e1
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-01-06 15:03:56 +0300

    add container MyPosts

--- src/components/Dialogs/DialogsItemContainer.tsx
+++ src/components/Dialogs/DialogsItemContainer.tsx
@@ -1,26 +1,9 @@
 import {Dialogs} from "./Dialogs";
 import {connect} from "react-redux";
-import store, {AppStateType} from "../Redux/Rudux_Store";
-import {addDialogCreatorAC, changeDialogsPostAC, TYPE_DISPATCH_CREATOR} from "../Redux/dialog_reducer";
-import {Message} from "./Message/Message";
+import {AppStateType} from "../Redux/Rudux_Store";
+import {addDialogCreatorAC, changeDialogsPostAC} from "../Redux/dialog_reducer"
 import React, {Dispatch} from "react";
 
-export type ActionPropsType = {
-    type: TYPE_DISPATCH_CREATOR,
-    value?: string
-}
-const sendMessage = () => {
-    store.dispatch(addDialogCreatorAC())
-}
-const updateNewMessafe = (value: string) => {
-    store.dispatch(changeDialogsPostAC(value))
-}
-export const DialogsItemContainer1 = () => {
-
-    // return (
-    //     // <Dialogs dialogNick={} messages={} messageBody={} dispatch={}/>
-    // )
-}
 
 let mapStateToProps = (store: AppStateType) => {
     return ({
@@ -32,13 +15,9 @@ let mapStateToProps = (store: AppStateType) => {
 let mapDispatchToProps = (dispatch: Dispatch<any>) => {
     return {
         updateNewMessageBody: (value: string) => {
-            dispatch(changeDialogsPostAC(value))
-        },
+            dispatch(changeDialogsPostAC(value))},
         sendMessage: () => {
-            dispatch(addDialogCreatorAC())
-        }
-
-
+            dispatch(addDialogCreatorAC())}
     }
 }
 
--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import p from './Message.module.css'
-import {Link} from "react-router-dom";
 
 type Mess = {
     message: string;
