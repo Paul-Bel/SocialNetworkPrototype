89c87ca38dd21a0fa6503df78ab316caf634d51b
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-01-17 13:25:43 +0300

    added test components

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const News = () => {
     return (
         <div>
-            <h1>Test News</h1>
+            <h1>Test News Components</h1>
         </div>
     )
 }
